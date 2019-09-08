d81c0d147c4f1ddca18f56a60d6e1a52af620791
RTHIND <thind_rajan@outlook.com>
2019-09-07 21:34:51 -0400

    tetris configuration

---
+++ settings.py
@@ -0,0 +1,24 @@
+cell_size = 18
+columns = 10
+rows = 22
+fps = 30
+
+#colors
+black = (0, 0, 0)
+red = (255, 17, 0)
+yellow = (255, 213, 0)
+blue = (0, 47, 255)
+light_blue = (0, 255, 234)
+green = (2, 212, 23)
+purple = (64, 0, 110)
+orange = (255, 149, 28)
+pink = (255, 82, 157)
+
+#tetris shapes
+red_shape = [[1, 1, 1], [0, 1, 0]]
+yellow_shape = [[0, 2, 2], [2, 2, 0]]
+blue_shape = [[3, 3, 0], [0, 3, 3]]
+light_blue_shape = [[4, 0, 0], [4, 4, 4]]
+green_shape = [[0, 0, 5], [5, 5, 5]]
+orange_shape = [[6, 6, 6, 6]]
+pink_shape = [[7, 7], [7, 7]]
