86c7d41a3545aa2ae8263aba168c43c2d1e768b5
RTHIND <thind_rajan@outlook.com>
2019-09-28 21:34:15 -0400

    added functions to the draw_text function

--- tetris.py
+++ tetris.py
@@ -15,7 +15,7 @@ def create_grid(locked_pos={}):
     #create one list for every row in grid
     #since we have 20 rows we want to create 20 sub lists
     #each sub list is going to have 10 colors 
-    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
+    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
     
     #drawing static blocks
     #columns (y) = j, rows (x) = i
@@ -25,15 +25,17 @@ def create_grid(locked_pos={}):
                 key = locked_pos[(j, i)]
                 grid[i][j] = key
     return grid
-                           
+ 
+################################################    
+    
 def convert_shape_format(shape): #takes the format of the block shapes and converts it into a form python can read
     pos = []
-    format = shape.shape[shape.rotation % len(shape.shape)]
+    form = shape.shape[shape.rotation % len(shape.shape)]
     #gives the sub list (the first list in the shape variables) needed
     #Ex. if shape.rotation = 0, then 0 % len(shape.shape) (len = 4) gives us the first list of the shape 
     #Ex. if shape.rotation = 1, then 1 % len(shape.shape) (len = 4) gives us the second list of the shape  
     
-    for i, line in enumerate(format):
+    for i, line in enumerate(form):
         row = list(line)
         for j, column in enumerate(row):
             if column == '0':
@@ -46,8 +48,10 @@ def convert_shape_format(shape): #takes the format of the block shapes and conve
     
     return pos
 
+###################################################
+
 def valid_space(shape, grid):
-    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (black)] for i in range(20)]
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
 #getting every single possible position for a 10x20 grid and adding it in a tuple
 #if grid[i][j] == black only adds the position of (j, i) if grid[i][j] == black is true
 
@@ -63,7 +67,8 @@ def valid_space(shape, grid):
             if pos[1] > -1:
                 return False
     return True
-    
+####################################################
+
 def check_lost(pos):
 #checks if any of the positions are above the screen
     for i in pos:
@@ -72,28 +77,37 @@ def check_lost(pos):
             return True
     
     return False
-        
+
+####################################################
+
 #picks one random shape falling down the screen 
 def get_shape():
     return Tetris(5, 0, random.choice(Shapes))
 
-def draw_text():
-    pass
+####################################################
 
-def draw_grid(Surface, grid):
-    for i in range(len(grid)):
+def draw_text(Surface, text, size, color):
+    label = TEXT.render(text, 1, color)
+    
+    Surface.blit(label, (X + board_width/2 - (label.get_width()/2), Y1 + board_height/2 - label.get_height()/2))
+ 
+####################################################
+
+def draw_grid(Surface, row, col):
+    for i in range(len(row)):
         #drawing horizontal lines
         pygame.draw.line(Surface, (gray), (X, Y + i*block_size), (X + board_width, Y + i*block_size))
      #draws 20 vertical lines   
      #everytime the function loops through a new row, the value of Y changes at the line that is being drawn, and X value stays static at the left side and right side of the screen
     
-        for j in range(len(grid[i])):
+        for j in range(col):
             #drawing vertical lines
             pygame.draw.line(Surface, (gray), (X + j*block_size, Y), (X + j*block_size, Y + board_height))
      #draws 10 horizontal lines
      #everytime the function loops through a new column, the value of X changes at the line that is being drawn, and the value of Y stays static at the top and bottom of the screen
             
-                       
+################################################    
+                                   
 def draw_window(Surface, grid):
     Surface.fill((black))
     
@@ -132,18 +146,17 @@ def draw_window(Surface, grid):
     #draws the actual grid
     #4 = border size
 
-    draw_grid(Surface, grid)        
+    draw_grid(Surface, row, col)        
     pygame.display.update() 
 
 ##################################################   
     
 def draw_next_shape(shape, Surface): #displays the next falling shape on the right side of the screen
-    text = TEXT
-    title = text.render('NEXT SHAPE', 1, (white))
+    title = TEXT.render('NEXT SHAPE', 1, (white))
     
     Sx = X + board_width + 50
     Sy = Y + board_height/2 - 100
-    format = shape.shape[shape.rotation % len(shape.shape)]
+    form = shape.shape[shape.rotation % len(shape.shape)]
     
     for i, line in enumerate(format):
         row = list(line)
@@ -155,6 +168,10 @@ def draw_next_shape(shape, Surface): #displays the next falling shape on the rig
     
 ###################################################
 
+def clear_row(grid, locked): #clears and shifts every row down when all positions are filled
+    
+    pass
+    
 
 ###################################################
 
@@ -195,26 +212,27 @@ def main(win):
                 quit()
             
             if event.type == pygame.KEYDOWN:
+                
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
-                    if not valid_space(current_piece, grid):
+                    if not(valid_space(current_piece, grid)):
                         current_piece.x += 1
                     
-                elif event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
-                    if not valid_space(current_piece, grid):
+                    if not(valid_space(current_piece, grid)):
                         current_piece.x -= 1
                 
                 if event.key == pygame.K_UP:
                     #rotating shape
-                    current_piece.rotation = current_piece.rotation + 1 % len(current.piece.shape)
-                    if not valid_space(current_piece, grid):
-                        current_piece.rotation =  current_piece.rotation - 1 % len(current_piece.shape)
+                    current_piece.rotation += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.rotation -= 1
                     
                 if event.key == pygame.K_DOWN:
                     #moving shape down
                     current_piece.y += 1
-                    if not valid_space(current_piece, grid):
+                    if not(valid_space(current_piece, grid)):
                         current_piece.y -= 1
 
 #the if not condition returns and checks if the current position of the piece is in a valid space
@@ -238,6 +256,8 @@ def main(win):
             current_piece = next_piece 
             next_piece = get_shape()
             change_piece = False
+            
+            clear_row(grid, locked_pos)
                         
         draw_window(win, grid)
         draw_next_shape(next_piece, win)
